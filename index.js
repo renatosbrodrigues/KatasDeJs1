30edacffaaa65a3e9a04164c5bdb75c2328176a5
Renato <renatosbrodrigues@gmail.com>
2022-05-03 10:25:42 -0300

    corrigindo atividade

--- index.js
+++ index.js
@@ -1,69 +1,98 @@
 
-for (let i = 0; i < 21; i++) {
-    console.log(i)
-}
 
+function oneThroughTwenty(num) {
 
-for (let i = 0; i < 21; i++) {
-
-    if (i % 2 == 0) {
-        console.log(i)
-    }
-
-}
-
-
-
-for (let i = 0; i < 21; i++) {
-
-    if (i % 2 !== 0) {
-       console.log(i)
-    }
-
-}
-
-
-
-for (let i = 0; i < 101; i++) {
-
-    if (i % 5 === 0) {
-       console.log(i)
-    }
-
-}
-
-
-
-for (let i = 0; i < 101; i++) {
-    let raizQuadrada = Math.sqrt(i)
-    if (Number.isInteger(raizQuadrada) === true) {
-       console.log(i)
-    }
-
-}
-
-
-
-for (let i = 20; i > 0 ; i--) {
-    console.log(i)
-}
-
-
-
-for (let i = 20; i > 0 ; i--) {
-    
-    if(i % 2 === 0){
-        console.log(i)
-    }
-    
-}
-
-
-for (let i = 20; i > 0 ; i--){
-    if (i % 2 !== 0){
+    for (let i = 0; i < num; i++) {
         console.log(i)
     }
 }
 
+function evensToTwenty(num) {
 
+    for (let i = 0; i < num; i++) {
 
+        if (i % 2 == 0) {
+            console.log(i)
+        }
+
+    }
+}
+
+function oddsToTwenty(num) {
+
+    for (let i = 0; i < num; i++) {
+
+        if (i % 2 !== 0) {
+            console.log(i)
+        }
+
+    }
+}
+
+function multiplesOfFive(num) {
+
+    for (let i = 0; i < num; i++) {
+
+        if (i % 5 === 0) {
+            console.log(i)
+        }
+
+    }
+}
+
+function squareNumbers(num) {
+
+    for (let i = 0; i < num; i++) {
+        let raizQuadrada = Math.sqrt(i)
+        if (Number.isInteger(raizQuadrada) === true) {
+            console.log(i)
+        }
+
+    }
+}
+
+function countingBackwards(num) {
+
+    for (let i = num; i > 0; i--) {
+        console.log(i)
+    }
+}
+
+function evenNumbersBackwards(num) {
+
+    for (let i = num; i > 0; i--) {
+
+        if (i % 2 === 0) {
+            console.log(i)
+        }
+
+    }
+}
+
+function oddNumbersBackwards(num) {
+
+    for (let i = num; i > 0; i--) {
+        if (i % 2 !== 0) {
+            console.log(i)
+        }
+    }
+}
+
+function multiplesOfFiveBackwards(num) {
+
+    for (let i = num; i > 0; i--) {
+        if (i % 5 === 0) {
+            console.log(i)
+        }
+    }
+}
+
+function squareNumbersBackwards(num) {
+
+    for (let i = 101; i > 0; i--) {
+        let raizQuadrada = Math.sqrt(i)
+        if (Number.isInteger(raizQuadrada) === true) {
+            console.log(i)
+        }
+    }
+}
